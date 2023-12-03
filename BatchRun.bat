d9db230215a6c476fce4c1da698e90ccaff226b1
ariz siddiqui <ariz.siddiqui@10pearls.com>
2023-12-04 03:02:47 +0500

    testng version upgrade

--- BatchRun.bat
+++ BatchRun.bat
@@ -1,4 +1,4 @@
 set projectLocation=C:\Users\Administrator\seleniumWD\parallel-universe
 cd C:\Users\Administrator\seleniumWD\parallel-universe
-mvn clean test -DxmlFilePath=C:\Users\Administrator\seleniumWD\parallel-universe\testng.xml,testngAndroidAppium.xml
+mvn clean test -DxmlFilePath=C:\Users\Administrator\seleniumWD\parallel-universe\testng.xml
 pause
